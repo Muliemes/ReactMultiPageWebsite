592de5c2889a8336faab83fa54c127f092cc4db4
MulieMes <mulemes81@gmail.com>
2022-05-24 15:48:23 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
-        <div class="container">
+    <div className="footer" >
+      <footer class="py-5  fixed-bottom">
+        <div class="container " style={{color:'#e3918c',}}>
           <p class="m-0 text-center text-white">
             Copyright &copy; 2022 VIP Sports Shop
           </p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+// import background from "././images/VipUI.png";
 function Home() {
   
   return (
     <div className="home">
       <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+  <div class="row align-items-center my-5">
+<div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
@@ -24,7 +24,7 @@ function Home() {
           </div>
         </div>
       </div>
-    </div>
+      </div>
   );
 }
 
